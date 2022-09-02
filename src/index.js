c528a8ee717eb532b78ba4f5b52ab113cc2e0b78
rao119349 <sauravyadav1193@gmail.com>
2022-09-02 23:27:15 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import { createRoot } from 'react-dom/client'; // for Version 18 And above
 import App from "./App";
 import './assets/styles/_main.scss';
 
-createRoot(document.getElementById('root')).render(<App/>) // for Version 18 And above
+// createRoot(document.getElementById('root')).render(<App/>) // for Version 18 And above
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>);
 // ReactDOM.render(<App /> , document.getElementById('root')); // for Version 17 And Below
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
+  // this.state ={name:''};
+  //this.setState({name: 'vipin1 '})
+  // promise
+
+  const [employeeName, setEmployeeName] = React.useState("");
+
+  const [covidData, setCovidData] = React.useState(null);
+  console.log(covidData && covidData[0]);
+  /*
+  ye use hota hai state initialise karne ko 
+  use state return two thing 
+  [employeeFirstName,setEmployeeName] this code is array desctructuring
+  */
+
+  /**useEffect
+   * / before rendering ie data base call and setting timeout register dom event
+   *
+   */
+  React.useEffect(() => {
+    fetch("https://data.covid19india.org/data.json")
+      .then((response) => response.json())
+      .then((resp) => setCovidData(resp.statewise));
+  }, []);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setEmployeeName(event.target.value);
+  };
+
   return (
     <>
-<h1>You are on Contact Page Now!</h1>
-    <p>Write down your contacts here.</p>
-</>
-  )
-}
+      <h1>You are on Contact Page Now!</h1>
+      <p>Write down your contacts here.</p>
+      <div>
+        <label> Sample</label>
+        <input
+          name="employeeName"
+          type="text"
+          value={employeeName}
+          onChange={handleChange}
+        />
 
-export default Contact
+        <label>{employeeName} </label>
+        <ul>
+          {covidData &&
+            covidData.map((object) => {
+              return (
+                <li key={object.statecode}>
+                  {object.statecode} has {object.active} cases{" "}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
